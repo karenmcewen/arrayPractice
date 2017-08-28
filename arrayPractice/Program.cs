f80d6214b3f459f902a5a19c6d5504628365e120
karenmcewen <karenf_email@yahoo.com>
2017-08-28 17:58:21 -0400

    first commit

---
+++ arrayPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring an array
+            //DataType[] nameOfArray = { elements};
+            int[] myArray = { 1, 2, 3, 4, 5, 6 }; //initialize the array 
+            string[] students = { "Brad", "Amy", "Jackie", "Rick", "Mike" };
+            Console.WriteLine(students[4]);
+            Console.WriteLine(students[0]);
+
+        
+
+        }
+    }
+}
